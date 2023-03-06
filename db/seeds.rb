08b5c3589de2c6335adbd11200dc4403bfc3980b
Baber Javed <baberjaved.rs@gmail.com>
2023-03-06 05:05:40 -0600

    Load data form csv in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+Company.destroy_all
+
+puts "database cleaned"
+
+file_path = Rails.root.join("lib", "crm_data.csv")
+person_columns = %w(first_name last_name email phone stage probability)
+
+CSV.foreach(file_path, headers: true) do |row|
+  company_name = row["company"].to_s.strip
+  company = Company.find_or_create_by(name: company_name)
+
+  person_attributes = row.to_h.slice(*person_columns)
+  person_attributes.merge!(company: company) if company.persisted?
+
+  person = Person.find_or_initialize_by(person_attributes)
+
+  puts "person already found: #{person.email}" and next if person.persisted?
+
+  if person.save
+    puts "person created: #{person.first_name} #{person.last_name}"
+  else
+    puts "person not created: #{person.errors.full_messages.to_sentence}"
+  end
+end
+
+puts Person.count
